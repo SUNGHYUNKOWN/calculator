7d261a47b40fcc2e5970cf93a6da5663efbf7fea
권성현 <tjdfus99532@gmail.com>
2025-12-13 02:12:58 +0900

    202211100 권성현 stack.h 1-1line update
    Add STUDENT_INFO_HEADER macro for OSS assignment 3.

--- stack.h
+++ stack.h
@@ -1,5 +1,6 @@
 #ifndef STACK_H
 #define STACK_H
+#define STUDENT_INFO_HEADER "202211100 권성현"
 #include <stdbool.h> 
 
 typedef int Item;
@@ -18,4 +19,5 @@ Item pop(Stack *s);
 Item peek(Stack *s);
 static void stack_underflow(void);
 static void reallocate(Stack * stack);
+
 #endif
